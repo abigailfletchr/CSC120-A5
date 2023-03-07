59a5148bcfe2eeea7e6a9538a6232ebf39c59f96
Your Name <you@example.com>
2023-03-07 00:21:09 -0500

    pants

--- Pants.java
+++ Pants.java
@@ -10,7 +10,12 @@ public class Pants {
      * TODO: Modify this method to print ASCII Art Pants
      */
     public void display() {
-        System.out.println("Pants?);
+        System.out.println(",==c==.");
+        System.out.println("|_/|\\_|");
+        System.out.println("| ´|` |");
+        System.out.println("|  |  |");
+        System.out.println("|  |  |");
+        System.out.println("|__|__|");
     }
 
     /* main method (for testing) */
